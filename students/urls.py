2aa6830b9da89663c09df516067195182bf11ec6
vqton <vuquangton@outlook.com>
2022-10-10 09:58:14 +0700

    Third commit

--- students/urls.py
+++ students/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:id>", views.view_student, name="view_student"),
+    path("add/", views.add, name="add"),
+    path("edit/<int:id>/", views.edit, name="edit"),
+    path("delete/<int:id>/", views.delete, name="delete"),
 ]
